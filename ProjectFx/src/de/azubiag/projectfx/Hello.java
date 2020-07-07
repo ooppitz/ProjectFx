9330286dda4c46e263b3c40799c76182bb3e862e
Benedikt-Greinwald <benedikt.greinwald@gmail.com>
2020-07-07 15:48:18 +0200

    Classpath changes

--- ProjectFx/src/de/azubiag/projectfx/Hello.java
+++ ProjectFx/src/de/azubiag/projectfx/Hello.java
@@ -21,7 +21,7 @@ public class Hello extends Application {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		System.out.println("Hello from Benedikt!");
 		launch(args);
 	}
 }
