1847e9527e6f1fd79da3f230ed9170502a029639
ooppitz <ooppitz@gmail.com>
2020-07-07 15:36:16 +0200

    Change layout of window to 400x400. Add empty CSS file.

--- ProjectFx/src/de/azubiag/projectfx/Hello.java
+++ ProjectFx/src/de/azubiag/projectfx/Hello.java
@@ -11,8 +11,8 @@ public class Hello extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,100,400);
-			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
